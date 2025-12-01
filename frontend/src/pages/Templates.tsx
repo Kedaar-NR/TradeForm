86f8611a3f6fe03351f8f3133be28d88dc445950
Kedaar-NR <kedaar.nandan@gmail.com>
2025-12-01 11:04:23 -0800

    template vercel deployment

--- frontend/src/pages/Templates.tsx
+++ frontend/src/pages/Templates.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { projectsApi, criteriaApi, projectGroupsApi, componentsApi } from "../services/api";
+import { projectsApi, criteriaApi, projectGroupsApi } from "../services/api";
 import { projectTemplates, studyTemplates } from "../data/templateData";
 import type { ProjectTemplate, StudyTemplate } from "../data/templateTypes";
 import { API_BASE_URL, getAuthToken } from "../utils/apiHelpers";
